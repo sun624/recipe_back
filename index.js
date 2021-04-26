a3021946f978123339272cdbf922d7ad39f30564
sun624 <67940809+sun624@users.noreply.github.com>
2021-04-26 13:26:37 -0700

    inital setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+//data from json
+app.use(express.json());
+//data from form
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}.`);
+});
