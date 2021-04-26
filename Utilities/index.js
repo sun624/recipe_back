c7d1031c7a6fe3217f9e6b411e76ca50b93f6cd5
sun624 <67940809+sun624@users.noreply.github.com>
2021-04-26 14:27:04 -0700

    add getRandomRecipe in Utility

---
+++ Utilities/index.js
@@ -0,0 +1,8 @@
+const foodAPI = "http://www.recipepuppy.com/api/";
+const foodAPI_query = "?i=onions,garlic&q=omelet&p=1";
+
+function getRandomRecipe(){
+
+    const recipeArray = ["burger","pasta","taco","salads","vegeterian","pizza","wing","sandwich","fries","hotdog"]
+
+}
