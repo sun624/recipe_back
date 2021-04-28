2995133a1239a2fa52dd42428a01256719e90151
Ethan <sunyichun624@gmail.com>
2021-04-27 22:52:01 -0700

    bug fix axios

--- Utilities/index.js
+++ Utilities/index.js
@@ -28,7 +28,7 @@ function getRandomFood() {
 async function getRecipe(food) {
   const foodAPI = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULER_API_KEY}&query=${food}&addRecipeInformation=true&instructionsRequired=true`;
 
-  const res = await axios(foodAPI);
+  const res = await axios.get(foodAPI);
   const foodCollection = res.data.results;
 
   return foodCollection[randIndex(foodCollection.length)];
