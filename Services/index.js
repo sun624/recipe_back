58371f8f2a08756bebc3afd0db2a1721907ebaea
sun624 <67940809+sun624@users.noreply.github.com>
2021-04-29 13:17:29 -0700

    delete unsplashAPI

--- Services/index.js
+++ Services/index.js
@@ -8,14 +8,8 @@ const axios = require("axios");
 async function getRecipefromMealDB(id) {
   intId = parseInt(id);
   const URL = `www.themealdb.com/api/json/v1/1/lookup.php?i=${intId}`;
-  
-  const fallBackUrl = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&page=1&query=${name}`;
-
   const res = await axios.get(URL);
   const recipes = res.data.meals;
-  if (recipes.length === 0) {
-    return fallBackUrl;
-  }
 
   return recipes[0];
 }
