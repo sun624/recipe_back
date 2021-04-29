6c501931974764b84630e2b638b3a18c2440ef4a
sun624 <67940809+sun624@users.noreply.github.com>
2021-04-29 13:16:07 -0700

    update

--- Services/index.js
+++ Services/index.js
@@ -4,20 +4,11 @@ const axios = require("axios");
 //   require("../Secrets");
 // }
 
-function getUID() {
-  let uid = "";
-  for (let i = 0; i < 6; i++) {
-    const rand = Math.floor(Math.random() * 10);
-    uid += rand;
-  }
 
-  return uid;
-}
-
-async function getImgfromMealDB(name) {
-  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
-  const { meals } = data;
-
+async function getRecipefromMealDB(id) {
+  intId = parseInt(id);
+  const URL = `www.themealdb.com/api/json/v1/1/lookup.php?i=${intId}`;
+  
   const fallBackUrl = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&page=1&query=${name}`;
 
   const res = await axios.get(URL);
@@ -25,10 +16,9 @@ async function getImgfromMealDB(name) {
   if (recipes.length === 0) {
     return fallBackUrl;
   }
-  const rand = Math.floor(Math.random() * recipes.length);
-  return recipes[rand].strMealThumb;
+
+  return recipes[0];
 }
 module.exports = {
-  getUID,
-  getPhoto: getImgfromMealDB,
+  getRecipe: getRecipefromMealDB,
 };
--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-const { getRecipe, getRandomFood, getId, getPhoto } = require("./Utilities");
-if (!process.env.PORT) {
-  require("./Secrets");
-}
+const { getRecipe } = require("./Services");
+// if (!process.env.PORT) {
+//   require("./Secrets");
+// }
 const MongoClient = require("mongodb").MongoClient;
-
 const app = express();
-app.use(cors());
-app.use(express.json()); //data from json
-app.use(express.urlencoded({ extended: true })); //data from form
-app.use(express.static("public"));
 
-const path = require("path");
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/auth.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-const connectionString = `${process.env.MONGODB_KEY}`;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}.`);
-});
-
+const connectionString =
+  "mongodb+srv://sun624:19900624@cluster0.yrtr8.mongodb.net/recipes?retryWrites=true&w=majority";
 MongoClient.connect(
   connectionString,
-  { useUnifiedTopology: true },
+  {
+    useUnifiedTopology: true,
+  },
   (err, client) => {
-    if (err) return console.error(err);
-    console.log("Connected to Database");
-    const db = client.db("recipe-finder");
+    app.use(cors());
+    app.use(express.json()); //data from json
+    app.use(express.urlencoded({ extended: true })); //data from form
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}.`);
+    });
+
+    console.log("connect to Database");
+
     const recipeColletion = client.db("recipes").collection("recipes-favs");
 
-    //GET / best practice is to use query parameters
+    //GET /
     app.get("/", (req, res) => {
       console.log("Inside Get");
       const { email } = req.body;
@@ -49,20 +43,20 @@ MongoClient.connect(
     //POST /
     app.post("/", async (req, res) => {
       console.log(req.body);
-      const { email, uid, recipe } = req.body;
+      const { email, mealId } = req.body;
 
-      if (!email || !recipe) {
-        return res.status(400).json({ error: "email and recipe are required" });
-      }
+      // if (!email || !recipe) {
+      //   return res.status(400).json({ error: "email and recipe are required" });
+      // }
 
       const newRecipe = {
         email,
-        uid: getUID(),
-        recipe,
+        mealId: mealId,
+        recipe: await getRecipe(mealId),
       };
       recipeColletion.insertOne(newRecipe);
 
-      res.send("sccuess");
+      res.send("Successfully Added to your favourates");
     });
 
     //PUT / :UPDATE operation
